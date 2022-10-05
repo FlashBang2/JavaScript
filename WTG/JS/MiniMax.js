1875aa326c8d301a232b5ce62712c2272c48cdc3
FlashBang2 <maconnowosci@gmail.com>
2022-10-05 16:38:06 +0200

    Minimax Skeleton

--- WTG/JS/MiniMax.js
+++ WTG/JS/MiniMax.js
@@ -1,11 +1,41 @@
-const nextTurn = () =>{
-    let available = [];
-    for (let i = 0; i < 3; i++){
-        for (let j = 0; j < 3; j++) {
-            if (board[i][j] == '') {
-                available.push({i, j});
+//Implement AI/Player Turn Order (needs Implementation)
+let scores = {
+    X:1,
+    O:-1,
+    TIE:0
+}
+
+const minimax = (board, depth, isMaximizing) =>{
+    //Check If Someone already Won (needs Implementation)
+    if (result !== null) {
+        return scores[result];
+    }
+    if (isMaximizing) {
+        let bestScore = -Infinity;
+        for (let i = 0; i < 3; i++){
+            for (let j = 0; j < 3; j++){
+                if (board[i][j] == ''){
+                    board[i][j] = ai;
+                    let score = minimax(board, depth + 1, false);
+                    board[i][j] = '';
+                    bestScore = max(score, bestScore);
+                }
             }
         }
+        return bestScore;
     }
-let 
+    else {
+        let bestScore = Infinity;
+        for (let i = 0; i < 3; i++){
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j] == '') {
+                    board[i][j] = human;
+                    let score = minimax(board, depth + 1, true);
+                    board[i][j] = '';
+                    bestScore = min(score, bestScore);
+                }
+            }
+            return bestScore
+        }
+    }
 }
