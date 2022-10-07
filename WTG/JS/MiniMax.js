3e1c7b3ce763bc527ce99471d2bdebb27e0b0c90
FlashBang2 <maconnowosci@gmail.com>
2022-10-07 07:09:04 +0200

    Small Improvments to AI

--- WTG/JS/MiniMax.js
+++ WTG/JS/MiniMax.js
@@ -1,14 +1,14 @@
 const turnAI = (board, currentPlayer) =>{
-    var availabeMoves = [];
-        for (var y = 0; y < board.length; y++)
+    let availabeMoves = [];
+        for (let y = 0; y < board.length; y++)
         {
             if (board[y].classList.contains(`playerO`) || board[y].classList.contains(`playerX`))
                 continue;
             availabeMoves.push(y);
         }
-    let move = availabeMoves[Math.floor(Math.random()*(availabeMoves.length-1))];
-    if (move != undefined)
-    {   
+    if (availabeMoves.length > 0)
+    { 
+        let move = availabeMoves[Math.floor(Math.random()*(availabeMoves.length-1))];
         board[move].innerText = currentPlayer;
         board[move].classList.add(`player${currentPlayer}`);
     }
