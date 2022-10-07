d91f9b5fa849470b1f6f4e6d6f04b607cfe158ce
Konrad1124 <konradurbanski2k@gmail.com>
2022-10-07 19:55:10 +0200

    NegaMax cdn

--- WTG/JS/NegaMax.js
+++ WTG/JS/NegaMax.js
@@ -1,6 +1,7 @@
 const turnAI = (board, currentPlayer) => {
+
     let availabeMoves = [];
-        for (let y = 0; y < board.length; y++)
+       for (let y = 0; y < board.length; y++)
         {
             if (board[y].classList.contains(`playerO`) || board[y].classList.contains(`playerX`))
                 continue;
@@ -8,24 +9,32 @@ const turnAI = (board, currentPlayer) => {
         }
     if (availabeMoves.length > 0)
     { 
-        let move = availabeMoves[Math.floor(Math.random()*(availabeMoves.length-1))];
+        /*let move = availabeMoves[Math.floor(Math.random()*(availabeMoves.length-1))];
         board[move].innerText = currentPlayer;
-        board[move].classList.add(`player${currentPlayer}`);
+        board[move].classList.add(`player${currentPlayer}`);*/
+        
+            let score = negamax(board, 2, -1, availabeMoves);
+            if (bestScore < score) 
+            {
+                bestScore = score;
+                move = tile;
+            }
     }
     shared.handleResultValidation();
     shared.changePlayer();
 }
 
-const negamax = (board, depth, color) =>{
+const negamax = (board, depth, color, availabeMoves) =>{
     if (depth == 0) {
-        return color*board.length;
+        return color;
     }
-    var value = -Infinity;
-    for (let i = 0; i < board.length; i++){
-            if (board[i] == ''){
-                value= Math.max(value, -negamax(board, depth-1, -color));
+    console.log(availabeMoves);
+    bestScore = -Infinity;
+    for (let i = 0; i < availabeMoves.length; i++){
+            if (board[i].textContent == ''){
+                bestScore= Math.max(bestScore, -negamax(board, depth-1, -color));
             }
     }
-
-   return value;
+    console.log(bestScore)
+   return bestScore;
 }
