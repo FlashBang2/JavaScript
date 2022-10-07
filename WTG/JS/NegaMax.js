f4e99c3c8635aa98c798f42e31cafac234f78cbe
Konrad1124 <konradurbanski2k@gmail.com>
2022-10-07 09:41:03 +0200

    NagaMax starting

--- WTG/JS/NegaMax.js
+++ WTG/JS/NegaMax.js
@@ -1,3 +1,21 @@
-const turnAI = () => {
-    
+const turnAI = (board, currentPlayer) => {
+    let availabeMoves = [];
+        for (let y = 0; y < board.length; y++)
+        {
+            if (board[y].classList.contains(`playerO`) || board[y].classList.contains(`playerX`))
+                continue;
+            availabeMoves.push(y);
+        }
+    if (availabeMoves.length > 0)
+    { 
+        let move = availabeMoves[Math.floor(Math.random()*(availabeMoves.length-1))];
+        board[move].innerText = currentPlayer;
+        board[move].classList.add(`player${currentPlayer}`);
+    }
+    shared.handleResultValidation();
+    shared.changePlayer();
+}
+
+const negamax = (board, depth, alpha, beta, isMaximizing) =>{
+   
 }
