1579a4aa7d3b4a12c04cc284a5fc478f8a82d95b
FlashBang2 <maconnowosci@gmail.com>
2022-10-09 18:48:45 +0200

    Adding PNS file and Negamax AI

--- WTG/JS/NegaMax.js
+++ WTG/JS/NegaMax.js
@@ -1,44 +1,24 @@
-/*const turnAINega = (board, currentPlayer) => {
-
+const negamax = (board, depth, alpha, beta, currentPlayer) =>{
     let availabeMoves = [];
-       for (let y = 0; y < board.length; y++)
-        {
-            if (board[y].classList.contains(`playerO`) || board[y].classList.contains(`playerX`))
-                continue;
-            availabeMoves.push(y);
-        }
-    if (availabeMoves.length > 0)
-    { 
-        let move = availabeMoves[Math.floor(Math.random()*(availabeMoves.length-1))];
-        board[move].innerText = currentPlayer;
-        board[move].classList.add(`player${currentPlayer}`);
-        
-            let score = negamax(board, 9, -1);
-            console.log(score)
-            if (bestScore < score) 
-            {
-                bestScore = score;
-                move = tile;
-            }
+    for (let y = 0;y < board.length;y++)
+    {
+        if (board[y].classList.contains(`playerO`) || board[y].classList.contains(`playerX`))
+            continue;
+        availabeMoves.push(y);
     }
-    shared.handleResultValidation();
-    shared.changePlayer();
+    if (shared.validateDiagonalLines(currentPlayer) || shared.validateResultColumns(currentPlayer) || shared.validateResultRows(currentPlayer))
+        return currentPlayer == 'X' ? 1 : -1;
+    if (availabeMoves.length == 0)
+        return 0;
+    bestScore = -Infinity;
+    for (let tile of availabeMoves)
+    {
+        board[tile].innerText = currentPlayer;
+        board[tile].classList.add(`player${currentPlayer}`);
+        score = -negamax(board, depth + 1, -alpha, -beta, () =>currentPlayer == 'X' ? 'O': 'X');
+        board[tile].innerText = '';
+        board[tile].classList.remove(`player${currentPlayer}`);
+        bestScore = Math.max(score, bestScore);
+    }
+    return bestScore;
 }
-
-const negamax = (board, depth, color) =>{
-    //console.log(depth+" poziom");
-    if (depth == 0|| board.length ==0) {
-        return color;
-    }
-    let bestScore = -Infinity;
-    for (let i = 0; i < board.length; i++){
-        if (board[i].textContent == ''){
-            board[i].textContent = "ai";
-            let score = negamax(board, depth-1, -color);
-            board[i].textContent = '';
-            bestScore= Math.max(bestScore,-score);
-        }
-                
-    }
-   return bestScore;
-}*/
