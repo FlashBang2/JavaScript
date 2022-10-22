4cf7d391ed5fa5d173ed2e59c76b1b688a687932
FlashBang2 <maconnowosci@gmail.com>
2022-10-22 08:46:26 +0200

    Couple changes to structure

--- WTG/2.0/JS/AI.js
+++ WTG/2.0/JS/AI.js
@@ -11,91 +11,92 @@ class AI{
     move()
     {
         let array = this.matrix.getAvailabeSpots();
-        if (array.length > 0 && this.matrix.isGameActive && this.AI == "Random")
+        switch (true)
         {
-            let indices = array[Math.floor(Math.random() * array.length)];
-            this.matrix.getMatrix()[indices.x][indices.y].DOM.innerText = this.matrix.getSide();
-            this.matrix.getMatrix()[indices.x][indices.y].DOM.classList.add(`player${this.matrix.getSide()}`);
-            this.matrix.getSide() == 'X' ? this.matrix.Xbits[ this.matrix.getMatrix()[indices.x][indices.y].getRow()][ this.matrix.getMatrix()[indices.x][indices.y].getColumn()] = 1: this.matrix.Obits[ this.matrix.getMatrix()[indices.x][indices.y].getRow()][ this.matrix.getMatrix()[indices.x][indices.y].getColumn()] = 1;
-            this.matrix.validate();
-            if (this.matrix.getWinner() != null)
-            {
-                this.matrix.isGameActive = false;
-                this.matrix.getWinner() == 'X' ? document.querySelector("#display").innerHTML = "Player <span class='playerX'>X</span> Won" : document.querySelector("#display").innerHTML = "Player <span class='playerO'>O</span> Won";
-            }
-            else
-            {
-                this.matrix.getMatrix()[indices.x][indices.y].setValue();
-                this.matrix.getMatrix()[indices.x][indices.y].setOccupied();
-                document.querySelector("#display-player").classList.remove(`player${this.matrix.getSide()}`);
-                this.matrix.setSide();
-                document.querySelector("#display-player").classList.add(`player${this.matrix.getSide()}`);
-                document.querySelector("#display-player").innerText = this.matrix.getSide();
-            }
-        }
-        if (array.length > 0 && this.matrix.isGameActive && this.AI == "Minimax")
-        {
-            this.maximizingPlayer = this.matrix.getSide();
-            for (let indices of array)
-            {
-                if (this.maximizingPlayer == 'X')
+            case (array.length > 0 && this.matrix.isGameActive && this.AI == "Random"):
+                let indices = array[Math.floor(Math.random() * array.length)];
+                this.matrix.getMatrix()[indices.x][indices.y].DOM.innerText = this.matrix.getSide();
+                this.matrix.getMatrix()[indices.x][indices.y].DOM.classList.add(`player${this.matrix.getSide()}`);
+                this.matrix.getSide() == 'X' ? this.matrix.Xbits[ this.matrix.getMatrix()[indices.x][indices.y].getRow()][ this.matrix.getMatrix()[indices.x][indices.y].getColumn()] = 1: this.matrix.Obits[ this.matrix.getMatrix()[indices.x][indices.y].getRow()][ this.matrix.getMatrix()[indices.x][indices.y].getColumn()] = 1;
+                this.matrix.validate();
+                if (this.matrix.getWinner() != null)
                 {
-                    this.matrix.Xbits[this.matrix.getMatrix()[indices.x][indices.y].getRow()][ this.matrix.getMatrix()[indices.x][indices.y].getColumn()] = 1;
-                    this.matrix.getMatrix()[indices.x][indices.y].setValue();
-                    this.matrix.getMatrix()[indices.x][indices.y].setOccupied();
-                    this.matrix.setSide();
-                    let score = this.minimax(document.querySelector("#Depth").value);
-                    this.matrix.Xbits[this.matrix.getMatrix()[indices.x][indices.y].getRow()][ this.matrix.getMatrix()[indices.x][indices.y].getColumn()] = 0;
-                    this.matrix.getMatrix()[indices.x][indices.y].setValue();
-                    this.matrix.getMatrix()[indices.x][indices.y].setOccupied();
-                    this.matrix.setSide();
-                    if (score > this.bestScore)
-                    {
-                        this.bestScore = score;
-                        let x = indices.x;
-                        let y = indices.y;
-                        this.bestMove = {x, y};
-                    }
+                    this.matrix.isGameActive = false;
+                    this.matrix.getWinner() == 'X' ? document.querySelector("#display").innerHTML = "Player <span class='playerX'>X</span> Won" : document.querySelector("#display").innerHTML = "Player <span class='playerO'>O</span> Won";
                 }
                 else
                 {
-                    this.matrix.Obits[this.matrix.getMatrix()[indices.x][indices.y].getRow()][ this.matrix.getMatrix()[indices.x][indices.y].getColumn()] = 1;
                     this.matrix.getMatrix()[indices.x][indices.y].setValue();
                     this.matrix.getMatrix()[indices.x][indices.y].setOccupied();
+                    document.querySelector("#display-player").classList.remove(`player${this.matrix.getSide()}`);
                     this.matrix.setSide();
-                    let score = this.minimax(document.querySelector("#Depth").value);
-                    this.matrix.Obits[this.matrix.getMatrix()[indices.x][indices.y].getRow()][ this.matrix.getMatrix()[indices.x][indices.y].getColumn()] = 0;
-                    this.matrix.getMatrix()[indices.x][indices.y].setValue();
-                    this.matrix.getMatrix()[indices.x][indices.y].setOccupied();
-                    this.matrix.setSide();
-                    if (score > this.bestScore)
+                    document.querySelector("#display-player").classList.add(`player${this.matrix.getSide()}`);
+                    document.querySelector("#display-player").innerText = this.matrix.getSide();
+                }
+                break;
+            case (array.length > 0 && this.matrix.isGameActive && this.AI == "Minimax"):
+                this.maximizingPlayer = this.matrix.getSide();
+                for (let indices of array)
+                {
+                    if (this.maximizingPlayer == 'X')
                     {
-                        this.bestScore = score;
-                        let x = indices.x;
-                        let y = indices.y;
-                        this.bestMove = {x, y};
+                        this.matrix.Xbits[this.matrix.getMatrix()[indices.x][indices.y].getRow()][ this.matrix.getMatrix()[indices.x][indices.y].getColumn()] = 1;
+                        this.matrix.getMatrix()[indices.x][indices.y].setValue();
+                        this.matrix.getMatrix()[indices.x][indices.y].setOccupied();
+                        this.matrix.setSide();
+                        let score = this.minimax(document.querySelector("#Depth").value);
+                        this.matrix.Xbits[this.matrix.getMatrix()[indices.x][indices.y].getRow()][ this.matrix.getMatrix()[indices.x][indices.y].getColumn()] = 0;
+                        this.matrix.getMatrix()[indices.x][indices.y].setValue();
+                        this.matrix.getMatrix()[indices.x][indices.y].setOccupied();
+                        this.matrix.setSide();
+                        if (score > this.bestScore)
+                        {
+                            this.bestScore = score;
+                            let x = indices.x;
+                            let y = indices.y;
+                            this.bestMove = {x, y};
+                        }
+                    }
+                    else
+                    {
+                        this.matrix.Obits[this.matrix.getMatrix()[indices.x][indices.y].getRow()][ this.matrix.getMatrix()[indices.x][indices.y].getColumn()] = 1;
+                        this.matrix.getMatrix()[indices.x][indices.y].setValue();
+                        this.matrix.getMatrix()[indices.x][indices.y].setOccupied();
+                        this.matrix.setSide();
+                        let score = this.minimax(document.querySelector("#Depth").value);
+                        this.matrix.Obits[this.matrix.getMatrix()[indices.x][indices.y].getRow()][ this.matrix.getMatrix()[indices.x][indices.y].getColumn()] = 0;
+                        this.matrix.getMatrix()[indices.x][indices.y].setValue();
+                        this.matrix.getMatrix()[indices.x][indices.y].setOccupied();
+                        this.matrix.setSide();
+                        if (score > this.bestScore)
+                        {
+                            this.bestScore = score;
+                            let x = indices.x;
+                            let y = indices.y;
+                            this.bestMove = {x, y};
+                        }
                     }
                 }
-            }
-            this.matrix.getMatrix()[this.bestMove.x][this.bestMove.y].DOM.innerText = this.matrix.getSide();
-            this.matrix.getMatrix()[this.bestMove.x][this.bestMove.y].DOM.classList.add(`player${this.matrix.getSide()}`);
-            this.matrix.getSide() == 'X' ? this.matrix.Xbits[ this.matrix.getMatrix()[this.bestMove.x][this.bestMove.y].getRow()][ this.matrix.getMatrix()[this.bestMove.x][this.bestMove.y].getColumn()] = 1: this.matrix.Obits[ this.matrix.getMatrix()[this.bestMove.x][this.bestMove.y].getRow()][ this.matrix.getMatrix()[this.bestMove.x][this.bestMove.y].getColumn()] = 1;
-            this.matrix.validate();
-            if (this.matrix.getWinner() != null)
-            {
-                this.matrix.isGameActive = false;
-                this.matrix.getWinner() == 'X' ? document.querySelector("#display").innerHTML = "Player <span class='playerX'>X</span> Won" : document.querySelector("#display").innerHTML = "Player <span class='playerO'>O</span> Won";
-            }
-            else
-            {
-                this.matrix.getMatrix()[this.bestMove.x][this.bestMove.y].setValue();
-                this.matrix.getMatrix()[this.bestMove.x][this.bestMove.y].setOccupied();
-                document.querySelector("#display-player").classList.remove(`player${this.matrix.getSide()}`);
-                this.matrix.setSide();
-                document.querySelector("#display-player").classList.add(`player${this.matrix.getSide()}`);
-                document.querySelector("#display-player").innerText = this.matrix.getSide();
-            }
-        } 
+                this.matrix.getMatrix()[this.bestMove.x][this.bestMove.y].DOM.innerText = this.matrix.getSide();
+                this.matrix.getMatrix()[this.bestMove.x][this.bestMove.y].DOM.classList.add(`player${this.matrix.getSide()}`);
+                this.matrix.getSide() == 'X' ? this.matrix.Xbits[ this.matrix.getMatrix()[this.bestMove.x][this.bestMove.y].getRow()][ this.matrix.getMatrix()[this.bestMove.x][this.bestMove.y].getColumn()] = 1: this.matrix.Obits[ this.matrix.getMatrix()[this.bestMove.x][this.bestMove.y].getRow()][ this.matrix.getMatrix()[this.bestMove.x][this.bestMove.y].getColumn()] = 1;
+                this.matrix.validate();
+                if (this.matrix.getWinner() != null)
+                {
+                    this.matrix.isGameActive = false;
+                    this.matrix.getWinner() == 'X' ? document.querySelector("#display").innerHTML = "Player <span class='playerX'>X</span> Won" : document.querySelector("#display").innerHTML = "Player <span class='playerO'>O</span> Won";
+                }
+                else
+                {
+                    this.matrix.getMatrix()[this.bestMove.x][this.bestMove.y].setValue();
+                    this.matrix.getMatrix()[this.bestMove.x][this.bestMove.y].setOccupied();
+                    document.querySelector("#display-player").classList.remove(`player${this.matrix.getSide()}`);
+                    this.matrix.setSide();
+                    document.querySelector("#display-player").classList.add(`player${this.matrix.getSide()}`);
+                    document.querySelector("#display-player").innerText = this.matrix.getSide();
+                }
+                break;
+        }
     }
 
     minimax(depth)
@@ -175,5 +176,15 @@ class AI{
             }
             return bestScore;
         }
+    }
+
+    negamax(depth)
+    {
+
+    }
+
+    PNS()
+    {
+
     }
 }
