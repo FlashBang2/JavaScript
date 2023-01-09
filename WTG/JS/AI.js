be5b21bfb442ec5ba65d369f8dea0837297838c9
Konrad1124 <konradurbanski2k@gmail.com>
2023-01-09 21:28:45 +0100

    Update AI.js

--- WTG/JS/AI.js
+++ WTG/JS/AI.js
@@ -364,7 +364,7 @@ class AI{
                 bestChild = current.children[index];
             }
             if (parentDrawnNode == undefined) continue;
-            parentDrawnNode.children[index].text.name = current.children[index].winrate ;
+            parentDrawnNode.children[index].text.name = current.children[index].winrate +"/"+ current.children[index].visits;
         }
         return bestChild;
     }
